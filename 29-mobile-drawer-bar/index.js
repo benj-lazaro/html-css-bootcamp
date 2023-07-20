580d6769488167136d6196f1305ada4eb18a16e7
benj-lazaro <benjie.work@gmail.com>
2023-07-20 15:34:26 +0800

    Section 29 animating mobile drawer

--- 29-mobile-drawer-bar/index.js
+++ 29-mobile-drawer-bar/index.js
@@ -1,4 +1,4 @@
-// Select the hamburger container element from the document
+// Select the container element holding the hamburger icon & mobile drawwer
 const hamburger = document.querySelector(".hamburger");
 
 // Select individual elements that comprise the hamburger icon
@@ -6,9 +6,14 @@ const bar1 = document.querySelector(".bar1");
 const bar2 = document.querySelector(".bar2");
 const bar3 = document.querySelector(".bar3");
 
-// Add a 'click' Event Listener to animate the bars that comprises the hamburger icon
+// Select container element holding the mobile drawer
+const mobileNav = document.querySelector(".mobileNav");
+
+// Add a 'click' Event Listener to animate the hamburger icon & mobile drawer
 hamburger.addEventListener("click", () => {
   bar1.classList.toggle("animateBar1");
   bar2.classList.toggle("animateBar2");
   bar3.classList.toggle("animateBar3");
+  mobileNav.classList.toggle("openDrawer");
 });
+
